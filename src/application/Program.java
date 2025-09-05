3676517b0bcf0d7d35d52b9fa2e5999e887735b3
Ricael <ricaelmenezes@gmail.com>
2025-09-05 10:32:09 -0300

    feat: update feat added

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,9 +14,10 @@ public class Program {
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Department department = new Department(2,null);
-		Seller newSeller = new Seller(null, "Rigael", "Rigael@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id: "+newSeller.getId());
+		Seller newSeller = sellerDao.findById(11);
+		newSeller.setName("Ricael Sim");
+		sellerDao.update(newSeller);
+		
 	}
 
 }
