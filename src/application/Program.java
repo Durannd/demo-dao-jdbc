85c62530e8034e9b708bafa3ae227ce35f101596
Ricael <ricaelmenezes@gmail.com>
2025-09-05 10:26:53 -0300

    feat: implements insert method for entity seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
-import java.util.List;
+import java.sql.Connection;
+import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,11 +13,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Department department = new Department(2, null);
-		List<Seller> seller = sellerDao.findByDepartments(department);
-		for(Seller obj : seller) {
-			System.out.println(obj);
-		}
+		Department department = new Department(2,null);
+		Seller newSeller = new Seller(null, "Rigael", "Rigael@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id: "+newSeller.getId());
 	}
 
 }
